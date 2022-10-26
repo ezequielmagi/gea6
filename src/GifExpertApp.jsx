a282fb5d7cc1e4128fe2233fc5a3adf25550fb56
Ezequiel Magi <ezequielmaggi@gmail.com>
2022-10-26 11:26:41 -0300

    Creando el gifexpertapp

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,10 @@
+export default function GifExpertApp (){
+  return(
+    <div>
+      <h1>Gif Expert App</h1>
+      {/* TODO: input */}
+      <hr />
+      {/* TODO: Listar categorias de busquedas */}
+    </div>
+  )
+}
